36160820846f1a2c3085015ae40b7893fb2ce404
DENNIS <denniskarikiu@gmail.com>
2023-07-19 12:00:01 +0300

    Arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
+console.log(multiply)
+
+const random = (Math.random() * 10) + 1
+console.log(random)
+
+const num3 = 19;
+const num4 = 5;
+const mod = num3 % num4
+console.log(mod)
+
+const max = Math.max(4,5,15,20,9)
+console.log(max)
